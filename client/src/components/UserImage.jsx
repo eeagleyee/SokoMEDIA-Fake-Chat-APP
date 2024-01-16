73486f714b9f476753f9d1076419b1d332d83fee
JOSEPH IZUU <eeagleyee@gmail.com>
2024-01-16 18:55:50 +0100

    first commit

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+import { Box } from "@mui/material";
+
+const UserImage = ({ image, size = "60px" }) => {
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+        alt="user"
+        src={`http://localhost:3001/assets/${image}`}
+      />
+    </Box>
+  );
+};
+
+UserImage.propTypes = {
+  image: PropTypes.string.isRequired,
+  size: PropTypes.string, // size is not required since it has a default value
+};
+
+export default UserImage;
