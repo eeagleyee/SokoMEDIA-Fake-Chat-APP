7ee62ec29a436c5816c0b949b2a0458d437019f3
JOSEPH IZUU <eeagleyee@gmail.com>
2024-01-17 15:41:48 +0100

    third comment

--- server/index.js
+++ server/index.js
@@ -70,6 +70,10 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to sokomedia backend");
+});
+
 /* MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
 mongoose
